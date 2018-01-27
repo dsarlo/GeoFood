5dce83ed713bad7c0ffffc4127b0e521c68a8061
Daniel Sarlo <dsarlo@buffalo.edu>
2018-01-26 22:45:04 -0500

    Initial Commit

---
+++ GeoFood/FoodContext.cs
@@ -0,0 +1,8 @@
+﻿
+namespace GeoFood
+{
+    class FoodContext //Context for communicating between property classes and the GUI.
+    {
+
+    }
+}
---
+++ GeoFood/FoodGui.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace GeoFood
+{
+    public partial class FoodGui : Form
+    {
+        public FoodGui()
+        {
+            InitializeComponent();//TODO Design GUI!
+        }
+    }
+}
