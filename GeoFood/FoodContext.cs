fd75ea17cffa9f0f07e2e406ce64e3582e63f6f1
Daniel Sarlo <dsarlo@buffalo.edu>
2018-01-27 15:28:30 -0500

    Context created and ready for YelpApi

--- GeoFood/FoodContext.cs
+++ GeoFood/FoodContext.cs
@@ -1,8 +1,39 @@
 ﻿
+using System.Device.Location;
+using GeoFood.Model;
+
 namespace GeoFood
 {
-    class FoodContext //Context for communicating between property classes and the GUI.
+    internal class FoodContext //Context for communicating between property classes and the GUI.
     {
+        private double _latitude, _longitude;
+        private GeoCoordinateWatcher _geoWatcher;
 
+        public FoodContext()
+        {
+            GetCurrentLocation();
+            //GatherListOfRestaurants();
+        }
+
+        private void GetCurrentLocation()
+        {
+            _geoWatcher = new GeoCoordinateWatcher(GeoPositionAccuracy.Default);
+            _geoWatcher.PositionChanged += PositionChanged;
+            _geoWatcher.Start();
+        }
+
+        private void PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            GeoCoordinate currentLocation = e.Position.Location;
+            _latitude = currentLocation.Latitude;
+            _longitude = currentLocation.Longitude;
+            _geoWatcher.Stop();
+        }
+
+        public Restaurant GetRandomRestaurant(string preference)
+        {
+            //use yelp api to create a randomized restaurant object based on the given preference here^
+            return null;
+        }
     }
 }
--- GeoFood/FoodGui.Designer.cs
+++ GeoFood/FoodGui.Designer.cs
@@ -31,8 +31,6 @@
             this._submitButton = new System.Windows.Forms.Button();
             this._prefLabel = new System.Windows.Forms.Label();
             this._foodPrefDrop = new System.Windows.Forms.ComboBox();
-            this._zipLabel = new System.Windows.Forms.Label();
-            this._zipTxtbox = new System.Windows.Forms.TextBox();
             this._bgPanel = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this._restPic = new System.Windows.Forms.PictureBox();
@@ -47,12 +45,13 @@
             // _submitButton
             // 
             this._submitButton.Font = new System.Drawing.Font("HelvLight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this._submitButton.Location = new System.Drawing.Point(117, 142);
+            this._submitButton.Location = new System.Drawing.Point(117, 83);
             this._submitButton.Name = "_submitButton";
             this._submitButton.Size = new System.Drawing.Size(154, 51);
             this._submitButton.TabIndex = 1;
             this._submitButton.Text = "Generate a Restaurant!";
             this._submitButton.UseVisualStyleBackColor = true;
+            this._submitButton.Click += new System.EventHandler(this._submitButton_Click);
             // 
             // _prefLabel
             // 
@@ -66,41 +65,35 @@
             // 
             // _foodPrefDrop
             // 
+            this._foodPrefDrop.Font = new System.Drawing.Font("HelvLight", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this._foodPrefDrop.FormattingEnabled = true;
+            this._foodPrefDrop.Items.AddRange(new object[] {
+            "American",
+            "Asian",
+            "German",
+            "Italian",
+            "Mediterranean",
+            "Polish",
+            "Seafood",
+            ""});
             this._foodPrefDrop.Location = new System.Drawing.Point(134, 38);
             this._foodPrefDrop.Name = "_foodPrefDrop";
-            this._foodPrefDrop.Size = new System.Drawing.Size(121, 21);
+            this._foodPrefDrop.Size = new System.Drawing.Size(121, 24);
             this._foodPrefDrop.TabIndex = 2;
-            // 
-            // _zipLabel
-            // 
-            this._zipLabel.AutoSize = true;
-            this._zipLabel.Font = new System.Drawing.Font("HelvLight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this._zipLabel.Location = new System.Drawing.Point(156, 75);
-            this._zipLabel.Name = "_zipLabel";
-            this._zipLabel.Size = new System.Drawing.Size(77, 19);
-            this._zipLabel.TabIndex = 4;
-            this._zipLabel.Text = "Zip Code";
-            // 
-            // _zipTxtbox
-            // 
-            this._zipTxtbox.Location = new System.Drawing.Point(144, 98);
-            this._zipTxtbox.Name = "_zipTxtbox";
-            this._zipTxtbox.Size = new System.Drawing.Size(100, 20);
-            this._zipTxtbox.TabIndex = 5;
             // 
             // _bgPanel
             // 
+            this._bgPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this._bgPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this._bgPanel.Controls.Add(this.panel2);
-            this._bgPanel.Controls.Add(this._zipTxtbox);
-            this._bgPanel.Controls.Add(this._zipLabel);
             this._bgPanel.Controls.Add(this._foodPrefDrop);
             this._bgPanel.Controls.Add(this._prefLabel);
             this._bgPanel.Controls.Add(this._submitButton);
-            this._bgPanel.Location = new System.Drawing.Point(12, 12);
+            this._bgPanel.Location = new System.Drawing.Point(12, 13);
             this._bgPanel.Name = "_bgPanel";
-            this._bgPanel.Size = new System.Drawing.Size(390, 338);
+            this._bgPanel.Size = new System.Drawing.Size(390, 275);
             this._bgPanel.TabIndex = 0;
             // 
             // panel2
@@ -111,14 +104,13 @@
             this.panel2.Controls.Add(this._restPrice);
             this.panel2.Controls.Add(this._restRate);
             this.panel2.Controls.Add(this._restName);
-            this.panel2.Location = new System.Drawing.Point(18, 217);
+            this.panel2.Location = new System.Drawing.Point(18, 154);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(353, 100);
             this.panel2.TabIndex = 6;
             // 
             // _restPic
             // 
-            this._restPic.BackgroundImage = global::GeoFood.Properties.Resources.restaurant_e1456862749354;
             this._restPic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this._restPic.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this._restPic.InitialImage = null;
@@ -134,9 +126,8 @@
             this._restPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this._restPrice.Location = new System.Drawing.Point(17, 63);
             this._restPrice.Name = "_restPrice";
-            this._restPrice.Size = new System.Drawing.Size(36, 20);
+            this._restPrice.Size = new System.Drawing.Size(0, 20);
             this._restPrice.TabIndex = 7;
-            this._restPrice.Text = "$$$";
             // 
             // _restRate
             // 
@@ -144,9 +135,8 @@
             this._restRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this._restRate.Location = new System.Drawing.Point(16, 38);
             this._restRate.Name = "_restRate";
-            this._restRate.Size = new System.Drawing.Size(82, 25);
+            this._restRate.Size = new System.Drawing.Size(0, 25);
             this._restRate.TabIndex = 6;
-            this._restRate.Text = "★★★★☆";
             // 
             // _restName
             // 
@@ -154,15 +144,14 @@
             this._restName.Font = new System.Drawing.Font("HelvLight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this._restName.Location = new System.Drawing.Point(17, 19);
             this._restName.Name = "_restName";
-            this._restName.Size = new System.Drawing.Size(171, 19);
+            this._restName.Size = new System.Drawing.Size(0, 19);
             this._restName.TabIndex = 5;
-            this._restName.Text = "Bippity Boop\'s Cucina";
             // 
             // FoodGui
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(414, 365);
+            this.ClientSize = new System.Drawing.Size(414, 302);
             this.Controls.Add(this._bgPanel);
             this.Name = "FoodGui";
             this.Text = "GeoFood";
@@ -180,8 +169,6 @@
         private System.Windows.Forms.Button _submitButton;
         private System.Windows.Forms.Label _prefLabel;
         private System.Windows.Forms.ComboBox _foodPrefDrop;
-        private System.Windows.Forms.Label _zipLabel;
-        private System.Windows.Forms.TextBox _zipTxtbox;
         private System.Windows.Forms.Panel _bgPanel;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label _restName;
